0c33207da439bb89ed376e5e75b189bb7c12c5be
Miguel-7-0 <clauseom@gmail.com>
2024-09-12 13:52:59 +0800

    Add files via upload

---
+++ LabChallenge2.java
@@ -0,0 +1,17 @@
+package labchallenge1;
+
+public class LabChallenge2 {
+
+    public static void main(String[] args) {
+        int length = 5, width = 7, area, perimeter;
+        perimeter = 2 * (length + width);
+        area = length * width;
+        System.out.println("This is my Laboratory Challenge No. 2: Calculating the Area and Perimeter of a Rectangle");
+        System.out.println("\nThe length of the garden is " + length + " meters.");
+        System.out.println("The width of the garden is " + width + " meters.");
+        System.out.println("The area of the garden is " + area + " meters.");
+        System.out.println("The paramater of the garden is " + perimeter + " meters.");
+
+    }
+}
+    
